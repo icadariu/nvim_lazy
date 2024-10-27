d0bc2e2f86053f9779bdbf02c703843f8dc0ceb4
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-10-27 11:32:04 +0100

    feat: updates
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -15,7 +15,7 @@ fi
 echo "Cloning nvim repository"
 mkdir -p ~/.config
 echo "Backup old nvim, if present"
-[ -d "$HOME/.config/nvim" ] && mv "$HOME/.config/nvim{.bk$RANDOM}"
+[ -d "$HOME/.config/nvim" ] && mv $HOME/.config/nvim $HOME/.config/nvim.bk_$RANDOM
 
 git clone https://github.com/icadariu/nvim.git ~/.config/nvim
 
