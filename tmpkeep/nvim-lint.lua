12ecb9dce8f24624fe3ebdc3d9969a8521f29512
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-10-29 00:08:29 +0100

    feat: lint not needed
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

---
+++ tmpkeep/nvim-lint.lua
@@ -0,0 +1,15 @@
+return {
+  "mfussenegger/nvim-lint",
+  events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+  linters_by_ft = {
+    -- https://github.com/mfussenegger/nvim-lint?tab=readme-ov-file#available-linters
+    terraform = { "tflint" },
+    ansiblelint = { "ansible_lint" },
+    bash = { "bash" },
+    markdownlint = { "markdownlint" },
+    tflint = { "tflint" },
+    tfsec = { "tfsec" },
+    yq = { "yq" },
+    zsh = { "zsh" },
+  },
+}
