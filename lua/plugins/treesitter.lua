89c8a0fa983ecfdd1e3159783c4062bbd80c598a
Ionut Cadariu <ionut.cadariu@gmail.com>
2024-10-29 09:27:18 +0100

    feat: added treesitter ignore lang
    Signed-off-by: Ionut Cadariu <ionut.cadariu@gmail.com>

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,33 @@
+-- https://github.com/nvim-treesitter/nvim-treesitter?tab=readme-ov-file#supported-languages
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "regex",
+      "lua",
+      "bash",
+      "diff",
+      "make",
+      "dockerfile",
+      "terraform",
+      "hcl",
+      "helm",
+      "tmux",
+      "jq",
+      "yaml",
+    },
+    ignore_install = {
+      "c",
+      "html",
+      "javascript",
+      "jsdoc",
+      "textproto",
+      "tsx",
+      "typescript",
+    },
+    sync_install = false,
+    auto_install = true,
+    indent = { enable = true },
+    highlight = { enable = true },
+  },
+}
